f733e545e0daac5c0ac4667060ea5819b1230e0b
kennedy-ak <akogokennedy@gmail.com>
2026-01-03 11:25:49 +0000

    update

--- eye_data_collection/eye_data_collection/settings.py
+++ eye_data_collection/eye_data_collection/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'eye_data_collection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='neondb'),
+        'USER': config('DB_USER', default='neondb_owner'),
+        'PASSWORD': config('DB_PASSWORD', default='npg_WgEePU71QSTq'),
+        'HOST': config('DB_HOST', default='ep-shy-bird-ahvumj5f-pooler.c-3.us-east-1.aws.neon.tech'),
+        'PORT': config('DB_PORT', default='5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
